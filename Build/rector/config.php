c32369bf1f1d038394f0ad31bd0a6a8165fed4d1
Oliver Klee <typo3-coding@oliverklee.de>
2026-08-17 19:16:53 +0200

    [TASK] Switch to Composer-based Rector sets

--- Build/rector/config.php
+++ Build/rector/config.php
@@ -3,24 +3,17 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\PHPUnit\Set\PHPUnitSetList;
-use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
 use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
-use Rector\ValueObject\PhpVersion;
 
 return RectorConfig::configure()
     ->withPaths([
         __DIR__ . '/../../src/',
         __DIR__ . '/../../tests/',
     ])
-    ->withPhpVersion(PhpVersion::PHP_83)
     ->withPhpSets()
+    ->withComposerBased(phpunit: true)
     ->withSets([
-        // Rector sets
-
-        // LevelSetList::UP_TO_PHP_83,
-
         // SetList::CODE_QUALITY,
         // SetList::CODING_STYLE,
         // SetList::DEAD_CODE,
@@ -29,12 +22,7 @@ return RectorConfig::configure()
         // SetList::NAMING,
         // SetList::PRIVATIZATION,
         // SetList::STRICT_BOOLEANS,
-        // SetList::TYPE_DECLARATION,
-
-        // PHPUnit sets
-
-        PHPUnitSetList::PHPUNIT_120,
-        // PHPUnitSetList::PHPUNIT_CODE_QUALITY,
+        SetList::TYPE_DECLARATION,
     ])
     ->withRules([
         AddVoidReturnTypeWhereNoReturnRector::class,
